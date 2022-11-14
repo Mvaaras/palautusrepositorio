15c608c74656fe2afea567c5ca34b1281cb83999
Vääräsmäki <mvaaras@gmail.com>
2022-11-14 13:28:36 +0200

    Tehtävät 1 ja 2 viikko 2

--- viikko2/project-reader/src/project_reader.py
+++ viikko2/project-reader/src/project_reader.py
@@ -1,10 +1,15 @@
 from urllib import request
 from project import Project
+from toml import loads
 
 
 class ProjectReader:
     def __init__(self, url):
         self._url = url
+    
+    def list_dependencies(dependencies, self):
+        return list(dependencies.keys())
+
 
     def get_project(self):
         # tiedoston merkkijonomuotoinen sisältö
@@ -12,4 +17,8 @@ class ProjectReader:
         print(content)
 
         # deserialisoi TOML-formaatissa oleva merkkijono ja muodosta Project-olio sen tietojen perusteella
-        return Project("Test name", "Test description", [], [])
+        toml_deserialized = loads(content)["tool"]["poetry"]
+        print(toml_deserialized)
+        dependencies = list(toml_deserialized["dependencies"].keys())
+        dev_dependencies = list(toml_deserialized["dev-dependencies"].keys())
+        return Project(toml_deserialized["name"], toml_deserialized["description"], dependencies, dev_dependencies)
