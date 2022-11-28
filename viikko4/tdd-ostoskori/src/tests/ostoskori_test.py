881c699dd9b083aff50546c14c5deb33272fd879
Vääräsmäki <mvaaras@gmail.com>
2022-11-28 23:41:29 +0200

    testi jos viimeinen tuote poistetaan, kori on tyhjä

--- viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
+++ viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
@@ -92,3 +92,13 @@ class TestOstoskori(unittest.TestCase):
         ostos = self.kori.ostokset()[0]
         self.assertEqual(ostos.tuotteen_nimi(), "Maito")
         self.assertEqual(ostos.lukumaara(), 1)
+
+    def test_jos_viimeinen_tuote_poistetaan_kori_on_tyhja(self):
+        maito = Tuote("Maito", 3)
+        self.kori.lisaa_tuote(maito)
+        self.kori.poista_tuote(maito)
+        self.assertEqual(self.kori.hinta(), 0)
+        self.assertEqual(self.kori.tavaroita_korissa(), 0)
+
+        ostokset = self.kori.ostokset()
+        self.assertEqual(len(ostokset), 0)
