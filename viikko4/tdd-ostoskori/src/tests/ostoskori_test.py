41eb558b598f5294161b1c8865f951cfec5343ca
Vääräsmäki <mvaaras@gmail.com>
2022-11-28 22:54:37 +0200

    testit useamman tuotteen lisääminen kasvattaa hintaa ja määrää

--- viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
+++ viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
@@ -19,3 +19,17 @@ class TestOstoskori(unittest.TestCase):
         maito = Tuote("Maito", 3)
         self.kori.lisaa_tuote(maito)
         self.assertEqual(self.kori.hinta(), 3)
+    
+    def test_kahden_tuotteen_lisaamisen_jalkeen_korissa_on_kaksi_tuotetta(self):
+        maito = Tuote("Maito",3)
+        leipa = Tuote("Leipa",5)
+        self.kori.lisaa_tuote(maito)
+        self.kori.lisaa_tuote(leipa)
+        self.assertEqual(self.kori.tavaroita_korissa(), 2)
+
+    def test_kahden_tuotteen_lisaamisen_jalkeen_korin_hinta_kasvaa(self):
+        maito = Tuote("Maito",3)
+        leipa = Tuote("Leipa",5)
+        self.kori.lisaa_tuote(maito)
+        self.kori.lisaa_tuote(leipa)
+        self.assertEqual(self.kori.hinta(), 8)
